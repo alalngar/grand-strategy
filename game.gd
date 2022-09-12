e84a2bb4c1a2d0a284da9cdc8750f5a6c5607f55
Jake0126 <62671778+SF-Jake@users.noreply.github.com>
2022-09-12 16:18:39 -0500

    Added monthly gold ui and icon

--- game.gd
+++ game.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal selected_province(data)
+signal monthly_income(data)
 
 var country := {}
 
@@ -11,7 +12,10 @@ func _monthly_tick():
 	_calc_tax()
 
 func _calc_tax():
+	var _monthly_income = 0.0
 	for province in Game.country.provinces:
 		# (((pop / 1000) + extra) / 12) * efficiency
 		var tax = ((float(province.population / 1000) + 0) / 12) * 1
 		Game.country.treasury += tax
+		_monthly_income += tax
+	monthly_income.emit(_monthly_income)
--- ui.gd
+++ ui.gd
@@ -4,7 +4,8 @@ extends CanvasLayer
 @onready var province_name := $Control/ProvincePanel/VBoxContainer/ProvinceName
 @onready var province_population := $Control/ProvincePanel/VBoxContainer/Population
 @onready var date := $Control/TimePanel/Date
-@onready var treasury := $Control/InfoPanel/MarginContainer/HBoxContainer/Money
+@onready var treasury := $Control/InfoPanel/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Money
+@onready var income := $Control/InfoPanel/MarginContainer/HBoxContainer/VBoxContainer/Income
 @onready var button_increase_speed = $Control/TimePanel/HBoxContainer/IncreaseSpeed
 @onready var button_decrease_speed = $Control/TimePanel/HBoxContainer/DecreaseSpeed
 @onready var button_pause_timer = $Control/TimePanel/HBoxContainer/PauseTimer
@@ -15,10 +16,11 @@ func _ready():
 	button_increase_speed.pressed.connect(_increase_speed)
 	button_decrease_speed.pressed.connect(_decrease_speed)
 	button_pause_timer.pressed.connect(_pause_timer)
+	Game.monthly_income.connect(monthly_income)
 
 func _process(delta):
 	date.text = Data.get_date_extended()
-	treasury.text = "Treasury: " + str(ceil(Game.country.treasury))
+	treasury.text = str(ceil(Game.country.treasury))
 	
 func _selected_province(data):
 	if data == null:
@@ -40,3 +42,11 @@ func _increase_speed():
 func _decrease_speed():
 	Data.time_speed = clamp(Data.time_speed + 0.2, 0.0, 1.0)
 	#game_speed.text = str(Data.time_speed)
+
+func monthly_income(data):
+	if (data >= 0.0):
+		income.text = "+" + str(data) + " Gold"
+	elif(data <= -0.1):
+		income.text = str(data) + " Gold"
+		
+
